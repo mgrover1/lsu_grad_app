8a2599d1fdb958e377079b52e72145dd6c4d8f7a
mgrover1 <maxi2312@gmail.com>
2019-02-03 21:15:38 -0600

    Create baton_rouge_temp_precip.py

---
+++ baton_rouge_temp_precip.py
@@ -0,0 +1,16 @@
+#Import the neccessary libraries
+import pandas as pd
+import bokeh as bokeh
+from bokeh.plotting import figure, show, output_file
+from bokeh.io import output_file, show, export_png
+from bokeh.models import FactorRange, ColumnDataSource, CDSView, IndexFilter
+from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
+from bokeh.layouts import column, layout, widgetbox
+from bokeh.layouts import layout
+import numpy as np
+import datetime as datetime,time
+from datetime import date
+import csv
+import urllib as urllib
+import json
+import matplotlib.pyplot as plt
