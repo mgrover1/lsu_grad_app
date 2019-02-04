a7e4ea5c8cf28cca6c937047b0b26d7518392035
mgrover1 <maxi2312@gmail.com>
2019-02-03 21:16:45 -0600

    Pull the json file and read it into the script

--- baton_rouge_temp_precip.py
+++ baton_rouge_temp_precip.py
@@ -14,3 +14,16 @@ import csv
 import urllib as urllib
 import json
 import matplotlib.pyplot as plt
+
+#Part 1 - Read in the data and create basic plots and export a text file 
+#Pull the json data provided by the Baton Rouge Airport 
+json_string = urllib.request.urlopen('http://data.rcc-acis.org/StnData?sid=kbtr&sdate=20180101&edate=20181231&elems=maxt,mint,pcpn&output=json')
+
+#Read in the json data and assign it to a variable to read 
+f = json_string.read()
+
+#Parse the json file to be read into a dataframe
+parsed_json = json.loads(f)
+
+#Extract the data from the parsed json file 
+data = parsed_json['data']
